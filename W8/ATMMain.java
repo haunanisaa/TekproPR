1962eec0b8ef7c545fef9b805f1a9fd61693f6a6
haunanisaa <78993323+haunanisaa@users.noreply.github.com>
2021-05-11 23:00:39 +0700

    Add files via upload

---
+++ W8/ATMMain.java
@@ -0,0 +1,13 @@
+package caseATM;
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
