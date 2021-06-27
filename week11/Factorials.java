88175208b51f259c757fe7b3a84ac668eb31adf0
haunanisaa <78993323+haunanisaa@users.noreply.github.com>
2021-06-27 18:16:13 +0700

    Add files via upload

---
+++ week11/Factorials.java
@@ -0,0 +1,27 @@
+package week11;
+//****************************************************************
+//Factorials.java
+//Reads integers from the user and prints the factorial of each.
+//
+//****************************************************************
+import java.util.Scanner;
+public class Factorials {
+	private static final MathUtils MathUtils = null;
+
+	public static void main(String[] args) {
+		String keepGoing = "y";
+		Scanner scan = new Scanner(System.in);
+		while (keepGoing.equals("y") || keepGoing.equals("Y")) {
+			System.out.print("Enter an integer: ");
+			int val = scan.nextInt();
+			try {
+				System.out.println("Factorial(" + val + ") = " + MathUtils.factorial(val));
+			}catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+			System.out.print("Another factorial? (y/n) ");
+			keepGoing = scan.next();
+		}
+		scan.close();
+	}
+}
---
+++ week11/ParseInts.java
@@ -0,0 +1,29 @@
+package week11;
+//****************************************************************
+//ParseInts.java
+//
+//Reads a line of text and prints the integers in the line.
+//
+//****************************************************************
+import java.util.Scanner;
+public class ParseInts {
+	public static void main(String[] args){
+		int val, sum=0;
+		Scanner scan = new Scanner(System.in);
+		String line;
+		
+		System.out.println("Enter a line of text");
+		Scanner scanLine = new Scanner(scan.nextLine());
+		
+		while (scanLine.hasNext()){
+			try{
+				val = Integer.parseInt(scanLine.next());
+				sum += val;
+			}catch(NumberFormatException e){
+				
+			}
+		}
+		System.out.println("\nThe sum of the integers on this line is " + sum);
+		scan.close();
+	}
+}
